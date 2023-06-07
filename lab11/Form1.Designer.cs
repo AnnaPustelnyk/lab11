a6fefc29d891fac277f3209417490ae62bf30a13
AnnaPustelnyk <anna46xk8@gmail.com>
2023-06-07 16:03:05 +0200

    commit2

--- lab11/Form1.Designer.cs
+++ lab11/Form1.Designer.cs
@@ -32,6 +32,10 @@
             radioButton2 = new RadioButton();
             button1 = new Button();
             button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
             SuspendLayout();
             // 
             // radioButton1
@@ -41,7 +45,6 @@
             radioButton1.Name = "radioButton1";
             radioButton1.Size = new Size(70, 24);
             radioButton1.TabIndex = 0;
-            radioButton1.TabStop = true;
             radioButton1.Text = "wojna";
             radioButton1.UseVisualStyleBackColor = true;
             radioButton1.CheckedChanged += radioButton1_CheckedChanged;
@@ -53,9 +56,9 @@
             radioButton2.Name = "radioButton2";
             radioButton2.Size = new Size(69, 24);
             radioButton2.TabIndex = 1;
-            radioButton2.TabStop = true;
             radioButton2.Text = "oczko";
             radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
             // 
             // button1
             // 
@@ -65,6 +68,8 @@
             button1.TabIndex = 2;
             button1.Text = "dobierz";
             button1.UseVisualStyleBackColor = true;
+            button1.Visible = false;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
@@ -74,18 +79,60 @@
             button2.TabIndex = 3;
             button2.Text = "stop";
             button2.UseVisualStyleBackColor = true;
+            button2.Visible = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(587, 344);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 20);
+            label1.TabIndex = 4;
+            label1.Text = "suma gracza 1:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(587, 375);
+            label2.Name = "label2";
+            label2.Size = new Size(107, 20);
+            label2.TabIndex = 5;
+            label2.Text = "suma gracza 2:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(700, 344);
+            label3.Name = "label3";
+            label3.Size = new Size(17, 20);
+            label3.TabIndex = 6;
+            label3.Text = "0";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(700, 375);
+            label4.Name = "label4";
+            label4.Size = new Size(17, 20);
+            label4.TabIndex = 7;
+            label4.Text = "0";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(radioButton2);
             Controls.Add(radioButton1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -96,5 +143,9 @@
         private RadioButton radioButton2;
         private Button button1;
         private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
--- lab11/Form1.cs
+++ lab11/Form1.cs
@@ -1,15 +1,62 @@
+﻿using System;
+
 namespace lab11
 {
     public partial class Form1 : Form
     {
+        private List<string> talia;
+        private Random random;
+        private int sumaGracza1;
+        private int sumaGracza2;
         public Form1()
         {
             InitializeComponent();
+            talia = new List<string>() { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+            talia = GenerujTalie(talia);
+            random = new Random();
+            sumaGracza1 = 0;
+            sumaGracza2 = 0;
+        }
+
+        private List<string> GenerujTalie(List<string> karty)
+        {
+            List<string> talia = new List<string>();
+            foreach (string karta in karty)
+            {
+                talia.Add(karta + " ♠");
+                talia.Add(karta + " ♣");
+                talia.Add(karta + " ♦");
+                talia.Add(karta + " ♥");
+            }
+            return talia;
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                button1.Visible = false;
+                button2.Visible = false;
+                label1.Visible = false;
+                label2.Visible = false;
+                label3.Visible = false;
+                label4.Visible = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
